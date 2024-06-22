a811fc10b18c01ea091d8b403c3e3d5adf057cf8
cwhite723 <c_white723@naver.com>
2024-06-22 21:47:01 +0900

    1주차 풀이 완료

--- 조하얀/1주차/Baekjoon_2346.java
+++ 조하얀/1주차/Baekjoon_2346.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class 백준2346 {
+public class Baekjoon_2346 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int totalBalloon = Integer.parseInt(br.readLine());
